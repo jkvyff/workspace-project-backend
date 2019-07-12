14fce40b3ca4efc3826d0c52ee5aaa20068ac198
cody <codyjeromegreen@gmail.com>
2019-07-12 09:36:11 -0700

    update

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,8 @@ module RailsAuthBackend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.action_cable.mount_path = '/cable'
+
+
   end
 end
